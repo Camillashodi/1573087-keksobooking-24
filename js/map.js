46ecda9424e639446c11012df86a15859851a6c4
Camillashodi <camillashodi@gmail.com>
2021-10-29 20:23:24 +0300

    adding handler DOMContentLoaded

--- js/map.js
+++ js/map.js
@@ -8,6 +8,76 @@ import { createAnnouncementPopup } from './create-popup-announcement.js';
 //35.6895
 //139.692
 
+document.addEventListener('DOMContentLoaded', () => {
+  const map = L.map('map-canvas')
+    .on('load', () => {
+      activateForm(adForm);
+      activateForm(mapForm);
+      adFormAddress.value = '35.68034, 139.76902';
+    })
+    .setView({
+      lat: 35.68034,
+      lng: 139.76902,
+    }, 14);
+
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  ).addTo(map);
+
+  const mainPinIcon = L.icon({
+    iconUrl: '../img/main-pin.svg',
+    iconSize: [50, 50],
+    iconAnchor: [25, 50],
+  });
+
+  const mainPinMarker = L.marker(
+    {
+      lat: 35.68034,
+      lng: 139.76902,
+    },
+    {
+      draggable: true,
+      icon: mainPinIcon,
+    },
+  );
+
+  mainPinMarker.addTo(map);
+
+  mainPinMarker.on('moveend', (evt) => {
+    adFormAddress.value = `${(evt.target.getLatLng().lat).toFixed(5)}, ${(evt.target.getLatLng().lng).toFixed(5)}`;
+  });
+
+  const announcements = getAnnouncements(quantityOfAnnouncements, checkins, checkouts, features, photos, types);
+  const template = document.querySelector('#card').content.querySelector('.popup');
+
+  announcements.forEach((announcement) => {
+    const {lat, lng} = announcement.location;
+
+    const icon = L.icon({
+      iconUrl: '../img/pin.svg',
+      iconSize: [50, 50],
+      iconAnchor: [25, 50],
+    });
+
+    const marker = L.marker(
+      {
+        lat,
+        lng,
+      },
+      {
+        icon,
+      },
+    );
+
+    marker
+      .addTo(map)
+      .bindPopup(createAnnouncementPopup(announcement, template));
+  });
+});
+/*
 const map = L.map('map-canvas')
   .on('load', () => {
     activateForm(adForm);
@@ -76,3 +146,4 @@ announcements.forEach((announcement) => {
     .addTo(map)
     .bindPopup(createAnnouncementPopup(announcement, template));
 });
+*/
