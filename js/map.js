3b272ca38a86f8813faafa9510ed085c8501864c
Camillashodi <camillashodi@gmail.com>
2021-11-02 02:28:36 +0300

    added ad-form popups and gettin data from server

--- js/map.js
+++ js/map.js
@@ -1,19 +1,57 @@
 import { activateForm } from './form-control.js';
 import { adForm, adFormAddress } from './ad-form.js';
 import { mapForm } from './map-form.js';
-import { checkins, checkouts, features, types, photos, quantityOfAnnouncements } from './initial-data.js';
-import { getAnnouncements } from './get-announcements.js';
+//import { checkins, checkouts, features, types, photos, quantityOfAnnouncements } from './initial-data.js';
+//import { getAnnouncements } from './get-announcements.js';
 import { createAnnouncementPopup } from './create-popup-announcement.js';
-//map-canvas
-//35.6895
-//139.692
+import { createLoaded } from './create-loaded.js';
 
-const map = L.map('map-canvas')
-  .on('load', () => {
-    activateForm(adForm);
-    activateForm(mapForm);
-    adFormAddress.value = '35.68034, 139.76902';
-  })
+const PIN_SIZE = 40;
+const MAIN_PIN_SIZE = 52;
+const DATA_HOST = 'https://24.javascript.pages.academy/keksobooking/data';
+const template = document.querySelector('#card').content.querySelector('.popup');
+const map = L.map('map-canvas');
+
+function showErrPopup (err) {
+  const errMapPopur = document.createElement('div');
+  errMapPopur.textContent = `Произошла ошибка: ${err}`;
+  errMapPopur.style.backgroundColor = 'red';
+  errMapPopur.style.width = '100%';
+  errMapPopur.style.textAlign = 'center';
+  document.querySelector('.map').prepend(errMapPopur);
+}
+
+function createMarkers (markerDataObjects) {
+  markerDataObjects.forEach((markerData) => {
+    const {lat, lng} = markerData.location;
+
+    const icon = L.icon({
+      iconUrl: './../img/pin.svg',
+      iconSize: [PIN_SIZE, PIN_SIZE],
+      iconAnchor: [(PIN_SIZE / 2), PIN_SIZE],
+    });
+
+    const marker = L.marker(
+      {
+        lat,
+        lng,
+      },
+      {
+        icon,
+      },
+    );
+
+    marker
+      .addTo(map)
+      .bindPopup(createAnnouncementPopup(markerData, template));
+  });
+}
+
+map.on('load', () => {
+  activateForm(adForm);
+  activateForm(mapForm);
+  adFormAddress.value = '35.68034, 139.76902';
+})
   .setView({
     lat: 35.68034,
     lng: 139.76902,
@@ -27,9 +65,9 @@ L.tileLayer(
 ).addTo(map);
 
 const mainPinIcon = L.icon({
-  iconUrl: '../img/main-pin.svg',
-  iconSize: [50, 50],
-  iconAnchor: [25, 50],
+  iconUrl: './../img/main-pin.svg',
+  iconSize: [MAIN_PIN_SIZE, MAIN_PIN_SIZE],
+  iconAnchor: [(MAIN_PIN_SIZE / 2), MAIN_PIN_SIZE],
 });
 
 const mainPinMarker = L.marker(
@@ -49,30 +87,6 @@ mainPinMarker.on('moveend', (evt) => {
   adFormAddress.value = `${(evt.target.getLatLng().lat).toFixed(5)}, ${(evt.target.getLatLng().lng).toFixed(5)}`;
 });
 
-const announcements = getAnnouncements(quantityOfAnnouncements, checkins, checkouts, features, photos, types);
-const template = document.querySelector('#card').content.querySelector('.popup');
-//console.log(announcements);
+createLoaded(createMarkers, showErrPopup, DATA_HOST);
 
-announcements.forEach((announcement) => {
-  const {lat, lng} = announcement.location;
-
-  const icon = L.icon({
-    iconUrl: '../img/pin.svg',
-    iconSize: [50, 50],
-    iconAnchor: [25, 50],
-  });
-
-  const marker = L.marker(
-    {
-      lat,
-      lng,
-    },
-    {
-      icon,
-    },
-  );
-
-  marker
-    .addTo(map)
-    .bindPopup(createAnnouncementPopup(announcement, template));
-});
+export { map, mainPinMarker };
